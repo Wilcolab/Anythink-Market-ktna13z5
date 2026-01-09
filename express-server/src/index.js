4c428d0bfd2cadfc040339a581d156ee8d11a079
T-RexBytes <azurebliss12@gmail.com>
2026-01-09 20:33:19 +0000

    add new express server

--- express-server/src/index.js
+++ express-server/src/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const PORT = 8001;
 
+// Root endpoint
+app.get('/', (req, res) => {
+    res.send('Express server is running!');
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+        console.log(`Server is running on http://localhost:${PORT}`);
 });
